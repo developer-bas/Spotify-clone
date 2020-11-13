01b20a206adfbfd3f6450616bc97419d7953eb53
Sebastian <cruzguzmaneric@gmail.com>
2020-11-13 05:14:02 -0600

    playButton animation finished

--- Spotify-clone/View/ContentItemView.swift
+++ Spotify-clone/View/ContentItemView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct ContentItemView: View {
     var topSpacerHeight: CGFloat = 400
+    var backColor = Color.init(red: 61/255, green: 89/255 , blue: 115/255)
    @State var playButtonOffSet: CGFloat = 335
     
     var body: some View {
         ZStack {
 //            Layer 0
 //            Color.init(red: 61/255, green: 89/255 , blue: 115/255) .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            LinearGradient(gradient: Gradient(colors: [Color.init(red: 61/255, green: 89/255 , blue: 115/255), Color.black,Color.black]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            LinearGradient(gradient: Gradient(colors: [ backColor, Color.black,Color.black]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
 //            Layer 1
             VStack{
                 Spacer().frame(height: 20)
@@ -66,24 +67,57 @@ struct ContentItemView: View {
                 }.background(Color.clear)
                 
             }
+            VStack{
+                LinearGradient(gradient: Gradient(colors: [ backColor, Color.clear]), startPoint: .top, endPoint: .bottom).frame(height: 200)
+                Spacer()
+            }.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
 //            Layer 3
             VStack {
                 Spacer()
                     .frame(height: playButtonOffSet + 300)
-                Text("Play")
-                    .foregroundColor(.white)
-                    .frame(width: 240, height : 50)
-                    .background(Color.init(red: 30/255, green: 215/255, blue: 96/255 ))
-                    .cornerRadius(25)
-                    .font(.system(size: 20, weight: .bold))
-                    .shadow(radius: 20)
+                
+                HStack{
+                    if(playButtonOffSet > -300) {
+                        Text("Play")
+                    }else{
+                        Image(systemName: "play.fill")
+                    }
+                }
+                .foregroundColor(.white)
+                .frame(width: getPlayButtonWidth(), height : 50)
+                .background(Color.init(red: 30/255, green: 215/255, blue: 96/255 ))
+                .cornerRadius(25)
+                .font(.system(size: 20, weight: .bold))
+                .shadow(radius: 20)
+                Spacer()
+            }
+//            Layer 4
+            VStack{
+                HStack{
+                    Image(systemName: "chevron.left")
+                    Spacer()
+                    Image(systemName: "ellipsis")
+                }
+                .foregroundColor(.white)
+                .padding(25)
                 Spacer()
             }
 //            Observer layer
-            VStack{
-                Text("\(playButtonOffSet)").foregroundColor(.yellow)
-                Spacer()
-            }
+//            VStack{
+//                Text("\(playButtonOffSet)").foregroundColor(.yellow)
+//                Spacer()
+//            }
+        }
+    }
+    
+    func getPlayButtonWidth()-> CGFloat{
+        if playButtonOffSet > -150{
+            return 240
+        } else if playButtonOffSet <= -300 {
+            return 50
+        }else{
+            var width: CGFloat = 240 - (190*(((-1 * playButtonOffSet) - 150)/150))
+            return width
         }
     }
 }
